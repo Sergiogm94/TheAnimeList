866db88bb9a99cf20b58b064ba95b111559006cc
Sergiogm94 <sergiogarcia1994@hotmail.com>
2026-05-02 18:20:14 +0200

    nav

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -7,10 +7,11 @@ import Buscador from "../Buscador/Buscador";
 export default function Nav() {
     const [usuario, setUsuario] = useState(null);
     const [scrolled, setScrolled] = useState(false);
-    const [menuOpen, setMenuOpen] = useState(false);
+    const [mostrarBuscador, setMostrarBuscador] = useState(false);
 
     const navigate = useNavigate();
 
+    // Función para obtener el usuario una vez que se ha logueado y mpostrarlo en el nav.
     useEffect(() => {
         const obtenerUsuario = async () => {
             try {
@@ -33,6 +34,7 @@ export default function Nav() {
         obtenerUsuario();
     }, []);
 
+    // Funcioón para hacer sticky en navbar.
     useEffect(() => {
         const handleScroll = () => {
             setScrolled(window.scrollY > 10);
@@ -51,68 +53,36 @@ export default function Nav() {
 
             setUsuario(null);
             navigate("/");
-            setMenuOpen(false);
 
         } catch (error) {
             console.log(error);
         }
     };
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-
-    const closeMenu = () => {
-        setMenuOpen(false);
+    //Función para mostrar el buscador.
+    const toggleSearch = () => {
+        setMostrarBuscador(!mostrarBuscador);
     };
 
     return (
         <div className={`container ${scrolled ? "scrolled" : ""}`}>
+            <ul className="nav-container">
 
-            <ul className={`nav-container ${menuOpen ? "open" : ""}`}>
-
-                <li>
-                    <NavLink to="/" onClick={closeMenu} className={({ isActive }) => isActive ? "active" : ""}>
-                        Home
-                    </NavLink>
-                </li>
-
-                <li>
-                    <NavLink to="/animes" onClick={closeMenu} className={({ isActive }) => isActive ? "active" : ""}>
-                        Animes
-                    </NavLink>
-                </li>
-
-                <li>
-                    <NavLink to="/personajes" onClick={closeMenu} className={({ isActive }) => isActive ? "active" : ""}>
-                        Personajes
-                    </NavLink>
-                </li>
-
-                <li>
-                    <NavLink to="/foro" onClick={closeMenu} className={({ isActive }) => isActive ? "active" : ""}>
-                        Foro
-                    </NavLink>
-                </li>
-
-                <li>
-                    <NavLink to="/perfil" onClick={closeMenu} className={({ isActive }) => isActive ? "active" : ""}>
-                        Perfil
-                    </NavLink>
-                </li>
+                <li><NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink></li>
+                <li><NavLink to="/animes" className={({ isActive }) => isActive ? "active" : ""}>Animes</NavLink></li>
+                <li><NavLink to="/personajes" className={({ isActive }) => isActive ? "active" : ""}>Personajes</NavLink></li>
+                <li><NavLink to="/foro" className={({ isActive }) => isActive ? "active" : ""}>Foro</NavLink></li>
+                <li><NavLink to="/perfil" className={({ isActive }) => isActive ? "active" : ""}>Perfil</NavLink></li>
 
                 {!usuario ? (
                     <li>
-                        <NavLink to="/loginreg" onClick={closeMenu}>
-                            Login/Registro
-                        </NavLink>
+                        <NavLink to="/loginreg">Login/Registro</NavLink>
                     </li>
                 ) : (
                     <>
-                        <li className="userData" onClick={() => { navigate("/perfil"); closeMenu(); }}>
+                        <li className="userData" onClick={() => navigate("/perfil")}>
                             👤 {usuario.usuario}
                         </li>
-
                         <li>
                             <button className="btnLogout" onClick={logout}>
                                 Logout
@@ -121,21 +91,26 @@ export default function Nav() {
                     </>
                 )}
 
-                <li className="nav-search">
-                    <Buscador />
+                <li className="spacer"></li>
+
+                {mostrarBuscador && (
+                    <li className="nav-search">
+                        <Buscador />
+                    </li>
+                )}
+
+                <li>
+                    <div
+                        className={`menu-icon ${mostrarBuscador ? "open" : ""}`}
+                        onClick={toggleSearch}
+                    >
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </li>
 
             </ul>
-
-            <div
-                className={`menu-icon ${menuOpen ? "open" : ""}`}
-                onClick={toggleMenu}
-            >
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-
         </div>
     );
 }
