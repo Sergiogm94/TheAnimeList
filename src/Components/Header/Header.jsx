8615b62bfd2eeb2ec5efc8f3bc739b275db57dff
Sergiogm94 <sergiogarcia1994@hotmail.com>
2026-05-02 19:22:19 +0200

    header act

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import "./header.css";
 import logo from "../../assets/Logo.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+    const navigate = useNavigate();
+
     return (
         <div className="header-container">
-            <img className="logo" src={logo} alt="logo" />
+            <img
+                className="logo"
+                src={logo}
+                alt="logo"
+                onClick={() => navigate("/")}
+                style={{ cursor: "pointer" }}
+            />
         </div>
     );
 }
